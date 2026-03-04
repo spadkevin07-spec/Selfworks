0e77c9dd4476112b8b533f3313518abab283c342
Kevin Spadaro <spadarokevin07@gmail.com>
2026-03-04 18:39:27 +0100

    Compito terminato

---
+++ swVARIABILI/main.js
@@ -0,0 +1,22 @@
+let num1 = 10;
+
+console.log(num1);
+
+let num2 = 20;
+
+console.log(num2);
+
+let stringa = 'Ciao';
+
+stringa = 'Hey!';
+
+console.log(stringa);
+
+const pGreco = 3.14159265359;
+
+pGreco = 787;
+
+console.log(pGreco);
+
+
+

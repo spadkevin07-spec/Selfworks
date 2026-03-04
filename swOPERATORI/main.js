9b4a2e08d91d2aef14d6b3bad5bb66736b24ff15
Kevin Spadaro <spadarokevin07@gmail.com>
2026-03-04 19:23:06 +0100

    Compito Terminato

---
+++ swOPERATORI/main.js
@@ -0,0 +1,14 @@
+let n1 = 10
+
+let n2 = 20
+
+let n3 = 30
+
+let n4 = 40
+
+let n5 = 50
+
+
+console.log(`La somma tra i numeri equivale a ${n1+n2+n3+n4+n5}`)
+
+console.log(`La media equivale a ${150/5}`)

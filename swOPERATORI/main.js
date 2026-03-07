f17daeab28a384e794fcc287c9dc92256119ccc3
Kevin Spadaro <spadarokevin07@gmail.com>
2026-03-07 15:35:48 +0100

    Compito 3 terminato

--- swOPERATORI/main.js
+++ swOPERATORI/main.js
@@ -1,16 +1,17 @@
-let annoCorrente = 2026;
+let num1 = 50;
 
-let dataNascita = 1990;
+let num2 = 35;
 
-let age = annoCorrente - dataNascita;
+let addizione = num1 + num2;
 
-console.log(age);
+let sottrazione = num1 - num2;
 
-let differenza = 100 - age;
+let moltiplicazione = num1 * num2;
 
-console.log(differenza);
+let divisione = num1 / num2;
 
-console.log(`Hai ${age} anni e ti mancano ${differenza} anni per arrivare a cento.`);
+let potenza = num1 ** num2;
 
+console.log(`I risultati delle varie operazioni sono: somma ${addizione}, sottrazione ${sottrazione}, moltiplicazione ${moltiplicazione}, divisione ${divisione}, elevato alla potenza ${potenza},`);
 
 

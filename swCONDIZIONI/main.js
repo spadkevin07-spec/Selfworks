d9e55e4f41ec6424712f285f472ecda2b1b4fae1
Kevin Spadaro <spadarokevin07@gmail.com>
2026-03-08 19:59:33 +0100

    Compito 1 terminato

---
+++ swCONDIZIONI/main.js
@@ -0,0 +1,69 @@
+// // let v = 29
+
+// // if(v < 18 && v >= 0){
+// //     console.log('Insufficiente');
+
+// // }else if(v >= 18 && v < 21){
+    
+// //     console.log('Sufficiente');
+
+// // }else if(v >= 21 && v < 24){
+    
+// //     console.log('Buono');
+
+// // }else if(v >= 24 && v < 27){
+    
+// //     console.log('Distinto');
+
+// // }else if(v >= 27 && v <= 29){
+    
+// //     console.log('Ottimo');
+
+// // }else if(v == 30){
+    
+// //     console.log('Eccellente');
+// // }else{
+//     console.log('voto non valido');
+//     }
+
+
+let voto = 18;
+
+let giudizio = 'insufficiente'
+
+switch(true){
+    case voto <= 18:
+        giudizio = 'insufficiente'
+        break;
+
+    case voto > 18 && voto <= 21:
+        giudizio = 'sufficiente'
+        break;
+    
+    case voto > 21 && voto <= 24:
+        giudizio = 'buono'
+        break;
+
+    case voto > 24 && voto <= 27:
+        giudizio = 'distinto'
+        break;
+
+    case voto > 27 && voto <= 29:
+        giudizio = 'ottimo'
+        break;
+
+    case voto == 30:
+        giudizio = 'eccellente'
+        break;
+
+    default:
+        giudizio = 'voto non valido'
+
+}
+
+console.log(giudizio);
+
+
+    
+
+    

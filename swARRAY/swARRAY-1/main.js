318ce306ceafab1f1461242616ef4856a560fe63
spadkevin07-spec <spadarokevin07@gmail.com>
2026-03-18 13:39:08 +0100

    Add files via upload

---
+++ swARRAY/swARRAY-1/main.js
@@ -0,0 +1,10 @@
+let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
+
+let ordineDecrescente = numbers.sort((a, b)=> b-a);
+
+console.log(ordineDecrescente);
+
+
+let ordineCrescente = numbers.sort((a, b)=> a-b);
+
+console.log(ordineCrescente);
